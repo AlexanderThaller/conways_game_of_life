512b4931c07d9bba641dbdba5a49b36a79b73e16
Alexander Thaller <alexander.thaller@trivago.com>
2017-09-02 19:40:36 +0200

    First rectangle.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [500, 500])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics| {
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0], // red
+                [0.0, 0.0, 10.0, 10.0],
+                context.transform,
+                graphics,
+            );
+        });
+    }
 }
